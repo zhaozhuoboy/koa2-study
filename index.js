2600187e7aec3bb78d2b6becc355a6b6e41f2145
zhaozhuoboy <zhaozhuoboy@163.com>
2017-12-26 16:46:39 +0800

    关闭cors

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const Koa = require('koa');
+const cors = require('@koa/cors');
 
 const app = new Koa();
-
+//关闭cors
+app.use(cors());
 // app.use(async ctx => {
 //     ctx.response.body ={msg:'服务器错误'};
 // });
--- router.js
+++ router.js
@@ -4,7 +4,7 @@ module.exports = function () {
     var router = new Router({
         prefix: '/api'
     })
-
+    
     router.get('/test', async ctx => {
         ctx.body={
             data:'test'
@@ -12,7 +12,6 @@ module.exports = function () {
     })
     router.post('/posts', async ctx => {
         //-POST 请求提交过来的参数在  ctx.request.body里
-        console.log(ctx.request.body)
         const bodys = ctx.request.body;
         ctx.body={
             data: bodys
